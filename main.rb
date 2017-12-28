2afd49f6bbdac8c7e4f9828de2ff5723dac16c7e
Sam Schirmer <samschirmer@gmail.com>
2017-12-28 16:06:20 -0600

    dkim rsa key working

--- main.rb
+++ main.rb
@@ -11,19 +11,44 @@ c = Secret::get
 
 def fetch_domains
 	domains = Array.new
-	result = @client.execute("select top 600 domain, spf, dkim, dmarc from bizops.dbo.tblemailauth where isnull(freeind, 0) = 0 and spf = 2")
+	# TODO suport DMARC and use this query instead
+#	result = @client.execute("select domain, spf, dkim, dmarc from bizops.dbo.tblemailauth where isnull(freeind, 0) = 0 and ((DATEDIFF(day, ISNULL(LastCheckDT, '01/01/2000'), GETUTCDATE()) > 2) or ((spf in (0,2)) or (dkim in (0,2)) or (dmarc in (0,2))))")
+	result = @client.execute("select domain, spf, dkim, dmarc from bizops.dbo.tblemailauth where isnull(freeind, 0) = 0 and ((DATEDIFF(day, ISNULL(LastCheckDT, '01/01/2000'), GETUTCDATE()) > 2) or ((spf in (0,2)) or (dkim in (0,2))))")
 	result.each(:symbolize_keys => true) do |row|
 		domains.push(row)
 	end
 	return domains
 end
 
-def lookup_dkim(type, url)
-	if type == 'key'
-		url = "dyn._domainkey.#{url}"
-	elsif type == 'policy'
-		url = "_domainkey.#{url}"
+#TODO check DKIM policy existence on _domainkey.domain.com
+def lookup_dkim(domains)
+	good_dkim = Array.new
+	bad_dkim = Array.new
+	counter = 0 
+
+	domains.each do |url|
+		puts "#{domains.count - counter} to go..."
+		counter += 1
+		selector = 'dyn._domainkey.' 
+
+		txts = Resolv::DNS.open do |dns|
+		  records = dns.getresources("#{selector}#{url}", Resolv::DNS::Resource::IN::TXT)
+		  records.empty? ? nil : records.map(&:data)
+		end
+		if txts.nil?
+			bad_dkim.push({url: url, note: 'No DKIM records on domain'})
+			next
+		else
+			txts.each do |txt|
+				if ((txt =~ /^k=rsa/) && (txt.length >= 224))
+					good_dkim.push(url)
+				else
+					bad_dkim.push({url: url, note: 'DKIM record malformed or too short'})
+				end
+			end
+		end
 	end
+	return good_dkim, bad_dkim
 end
 
 def lookup_spf(domains)
@@ -81,7 +106,7 @@ def record_result(valid, type, array)
 		if record.is_a? String
 			sql = "UPDATE BizOps.dbo.tblEmailAuth SET #{type} = #{valid}, LastCheckDT = GETUTCDATE() WHERE Domain = '#{record}'"
 		else
-			sql = "UPDATE BizOps.dbo.tblEmailAuth SET #{type} = #{valid}, ProblemText = '#{record[:note]}', LastCheckDT = GETUTCDATE() WHERE Domain = '#{record[:url]}'"
+			sql = "UPDATE BizOps.dbo.tblEmailAuth SET #{type} = #{valid}, ProblemText = CONCAT(ISNULL(ProblemText,''), ' | ', '#{record[:note]}'), LastCheckDT = GETUTCDATE() WHERE Domain = '#{record[:url]}'"
 		end	
 		result = @client.execute(sql).do
 	end
@@ -91,6 +116,7 @@ domains = fetch_domains
 spf_todo = Array.new	
 dkim_todo = Array.new	
 dmarc_todo = Array.new	
+# TODO will need to check old records eventually; possibly just when in (2,0)
 domains.each do |d|
 	if d[:spf] == 2
 		spf_todo.push(d[:domain])
@@ -109,6 +135,12 @@ good_spfs, bad_spfs = lookup_spf(spf_todo)
 puts "Now writing to BizOps..."
 record_result(1, 'SPF', good_spfs)
 record_result(0, 'SPF', bad_spfs)
+
+puts "SPF completed! Now processing DKIM..."
+good_dkims, bad_dkims = lookup_dkim(dkim_todo)
+puts "Writing DKIM results to BizOps..."
+record_result(1, 'DKIM', good_dkims)
+record_result(0, 'DKIM', bad_dkims)
 
 puts 'Done!'
 
